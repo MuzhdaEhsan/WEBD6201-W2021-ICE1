7dc3fa5fe43e81581ebf4a9978c00a69d206c180
Muzhda Ehsan <muzhda.ehsan@dcmail.ca>
2021-01-22 00:19:16 -0500

    initial commit

---
+++ Week1/Scripts/app.js
@@ -0,0 +1,24 @@
+/* custom scripts go here */
+
+//IIFE - Immediately Invoked Function Expression
+//AKA: Anonymous Self-Executing Function
+
+(function()
+{
+    // initialization function
+    function Start()
+    {
+        console.log("App Started...");
+
+        let clickMeButton = document.getElementById("clickMeButton");
+
+        clickMeButton.addEventListener("click", ()=> 
+        {
+            console.log("Click Me Button was clicked!");
+        });
+    }
+    
+    // event listener
+    window.addEventListener("load", Start);
+
+})();
